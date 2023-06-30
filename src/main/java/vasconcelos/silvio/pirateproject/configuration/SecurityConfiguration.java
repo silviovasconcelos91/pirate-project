43f3091482715418f1ceab20a96d44a1e50f65af
Maeils <silvio.vasc3@gmail.com>
2023-06-30 19:38:34 +0200

    Spring security
    Depuis la version 2.7 de Spring Boot "extends WebSecurityConfigurerAdapter" est déprécié. En effet Spring souhaite que la partie security ai plus une architecture basé sur les composants.

--- src/main/java/vasconcelos/silvio/pirateproject/configuration/SecurityConfiguration.java
+++ src/main/java/vasconcelos/silvio/pirateproject/configuration/SecurityConfiguration.java
@@ -2,26 +2,19 @@ package vasconcelos.silvio.pirateproject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("silvio").password(passwordEncoder().encode("silvio"))
-                .roles("USER");
-    }
+public class SecurityConfiguration {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    UserDetailsManager userDetailsManager() {
+        UserDetails silvioDetails = User.withDefaultPasswordEncoder().username("silvio").password("silvio").roles("USERS").build();
+        return new InMemoryUserDetailsManager(silvioDetails);
     }
 }
